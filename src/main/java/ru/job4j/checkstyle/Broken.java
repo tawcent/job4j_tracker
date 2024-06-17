2a0ac16ec0b9a0788420a10c80888c6ecf051f5a
tawcent <rusx745@gmail.com>
2024-06-17 21:06:55 +0400

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+    public String surname;
+    private final int sizeOfempty = 10;
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+
+        void method ( int a, int b) {
+        }
+
+
+        String Name;
+
+        Broken() {
+        }
+
+    }
+}
