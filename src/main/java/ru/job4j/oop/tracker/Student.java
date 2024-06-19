1712413420417eb6f0f85beccaee965207f8c7d4
tawcent <rusx745@gmail.com>
2024-06-19 17:13:58 +0400

    1.2. Вызов метода объекта

---
+++ src/main/java/ru/job4j/oop/tracker/Student.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop.tracker;
+
+public class Student {
+
+    public void sing() {
+        System.out.println("I believe i can fly");
+    }
+
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.sing();
+        petya.sing();
+        petya.sing();
+    }
+}
