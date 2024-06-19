dd70db8c2cf236bb14f19b3faada1880ec8cce02
tawcent <rusx745@gmail.com>
2024-06-19 19:19:37 +0400

    1.2. Вызов метода объекта

--- src/main/java/ru/job4j/oop/tracker/Cat.java
+++ src/main/java/ru/job4j/oop/tracker/Cat.java
@@ -2,14 +2,32 @@ package ru.job4j.oop.tracker;
 
 public class Cat {
 
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    private String food;
+
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + " eat "  + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There is gav's food");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("Kotleta");
+        gav.show();
+        System.out.println("There is black's food");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("Fish");
+        black.show();
     }
 }
