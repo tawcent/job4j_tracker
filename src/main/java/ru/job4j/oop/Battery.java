238346fa7f16f3342251606083b097813f80d01c
tawcent <rusx745@gmail.com>
2024-06-24 21:01:17 +0400

    1.2. Вызов метода объекта

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,28 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public String about() {
+        return "My charge: " + load + "%";
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(80);
+        Battery second = new Battery(20);
+        System.out.println("First : " + first.about());
+        System.out.println("Second : " + second.about());
+        first.exchange(second);
+        System.out.println("First : " + first.about());
+        System.out.println("Second : " + second.about());
+    }
+}
