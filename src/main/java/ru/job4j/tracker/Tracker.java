a2cd3dac67735072c8d5c30a67e8e5bd9377532b
tawcent <rusx745@gmail.com>
2024-07-07 17:29:19 +0400

    1.2. Вызов метода объекта

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,16 +13,20 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int result = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                result = index;
                 break;
             }
         }
-        return rsl;
+            return result;
+    }
+
+    public Item findById(int id) {
+       int index = indexOf(id);
+       return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
@@ -41,5 +45,15 @@ public class Tracker {
         }
         copy = Arrays.copyOf(copy, count);
         return copy;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            item.setId(id);
+            items[index] = item;
+        }
+        return result;
     }
 }
