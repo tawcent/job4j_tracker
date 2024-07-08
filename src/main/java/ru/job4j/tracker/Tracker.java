ccd7a4ff882d4f955563e1f1e84fa36e9b5df1f7
tawcent <rusx745@gmail.com>
2024-07-08 19:28:18 +0400

    1.2. Вызов метода объекта

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,10 +60,7 @@ public class Tracker {
     public void delete(int id) {
         int index = indexOf(id);
         if (index >= 0) {
-            int start = index + 1;
-            int distPos = index;
-            int lenght = size - index - 1;
-            System.arraycopy(items, start, items, distPos, lenght);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
