b6dba9c31c78ef4387d17898a14acd4901ec7b43
tawcent <rusx745@gmail.com>
2024-07-21 12:52:26 +0400

    1.2. Вызов метода объекта

--- src/main/java/ru/job4j/tracker/StartUi.java
+++ src/main/java/ru/job4j/tracker/StartUi.java
@@ -1,8 +1,35 @@
 package ru.job4j.tracker;
 
+import java.util.Scanner;
+
 public class StartUi {
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Выбрать: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
+
+    }
+
+    private void showMenu() {
+        String[] menu = {"Добавить новую заявку", "Показать все заявки", "Изменить заявку", "Удалить заявку", "Показать заявку по id", "Показать заявки по имени", "Завершить программу"};
+
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+
+    }
+
     public static void main(String[] args) {
-        Item lsd = new Item();
-        System.out.println(lsd);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUi().init(scanner, tracker);
     }
 }
