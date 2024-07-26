6d016632a069662d5bbb4946aeca68da6f4e59db
tawcent <rusx745@gmail.com>
2024-07-26 22:47:40 +0400

    1.2. Вызов метода объекта

--- src/test/java/ru/job4j/tracker/StartUiTest.java
+++ src/test/java/ru/job4j/tracker/StartUiTest.java
@@ -2,16 +2,21 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 class StartUiTest {
     @Test
     void whenAddItem() {
-        String[] answers = {"Fix PC"};
+        String[] answers = {"Fix PC", "Fix Bug"};
         Input input = new MockInput(answers);
         Tracker tracker = new Tracker();
         StartUi.createItem(input, tracker);
+        StartUi.createItem(input, tracker);
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
+        assertThat(created.getName()).isEqualTo(expected.getName());
+        created = tracker.findAll()[1];
+        expected = new Item("Fix Bug");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
 
