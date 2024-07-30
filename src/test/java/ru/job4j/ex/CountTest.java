a111e4ac4829ad4c58b1f3a7c2503ead7ee2479f
tawcent <rusx745@gmail.com>
2024-07-30 22:04:48 +0400

    1.2. Вызов метода объекта

---
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CountTest {
+    @Test
+    public void whenException() {
+        int start = 0;
+        int finish = 3;
+        int expected = 3;
+        int result = Count.add(start, finish);
+        assertThat(result).isEqualTo(expected);
+    }
+}
