1757461665e1f793ec8ea5f89faecb47ffb1734c
tawcent <rusx745@gmail.com>
2024-09-26 21:19:14 +0400

    Collection

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -16,7 +16,7 @@ public class Profiles {
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .sorted((a1, a2) -> a1.getCity().compareTo(a2.getCity()))
+                .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .collect(Collectors.toList());
     }
