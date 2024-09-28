f4edfecc42aff4709d24e83449e318bd84c512f8
tawcent <rusx745@gmail.com>
2024-09-28 18:45:52 +0400

    Collection

---
+++ src/main/java/ru/job4j/stream/Card.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return suit + " " + value;
+    }
+
+    public static void main(String[] args) {
+        Stream.of(Suit.values())
+                .flatMap(suit -> Stream.of(Value.values())
+                        .map(value -> new Card(suit, value)))
+                .forEach(card -> System.out.println(card));
+    }
+}
+
