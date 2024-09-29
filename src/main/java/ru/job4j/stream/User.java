d2dfefd9f51b54c22defa987af3ee4c70bdc4745
tawcent <rusx745@gmail.com>
2024-09-29 21:15:11 +0400

    Collection

---
+++ src/main/java/ru/job4j/stream/User.java
@@ -0,0 +1,58 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+/**
+ * Класс описывает модель пользователя банка.
+ * @author DAUTOV RUSLAN
+ * @version 1.0
+ */
+
+public class User {
+    private String passport;
+    private String username;
+
+    /**
+     *
+     * @param passport номер паспорта пользователя
+     * @param username имя пользователя
+     */
+
+    public User(String passport, String username) {
+        this.passport = passport;
+        this.username = username;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(passport, user.passport);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(passport);
+    }
+}
