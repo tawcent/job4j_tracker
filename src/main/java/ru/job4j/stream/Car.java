ad9da4d1e531bb5e08c53a229a6a24543140f3ac
tawcent <rusx745@gmail.com>
2024-09-30 19:17:51 +0400

    Collection

--- src/main/java/ru/job4j/stream/Car.java
+++ src/main/java/ru/job4j/stream/Car.java
@@ -35,38 +35,38 @@ public class Car {
         private String color;
 
         Builder buildBrand(String brand) {
-        this.brand = brand;
-        return this;
+            this.brand = brand;
+            return this;
         }
 
         Builder buildModel(String model) {
-        this.model = model;
-        return this;
+            this.model = model;
+            return this;
         }
 
         Builder buildCreated(LocalDate created) {
-        this.created = created;
-        return this;
+            this.created = created;
+            return this;
         }
 
         Builder buildVolume(double volume) {
-        this.volume = volume;
-        return this;
+            this.volume = volume;
+            return this;
         }
 
         Builder buildColor(String color) {
-        this.color = color;
-        return this;
+            this.color = color;
+            return this;
         }
 
         Car build() {
-        Car car = new Car();
-        car.brand = brand;
-        car.model = model;
-        car.created = created;
-        car.volume = volume;
-        car.color = color;
-        return car;
+            Car car = new Car();
+            car.brand = brand;
+            car.model = model;
+            car.created = created;
+            car.volume = volume;
+            car.color = color;
+            return car;
         }
     }
 
